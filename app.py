d2011a19dc2200a0921d5f430cc68171111b4e9b
Armanul Alam <65443530+armanulalam@users.noreply.github.com>
2024-07-18 02:18:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    # if request.method == "POST":
+    #     input_text = request.form['description']
+    #     output = predict_categories(input_text)[0]
+    #     confidence_list = output['confidences']
+    #     labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.5 ]
+    #     label_text = ""
+    #     for idx, label in enumerate(labels):
+    #         label_text = label_text + label
+    #         if idx != len(labels)-1: label_text = label_text + ", "
+    #     return render_template("result.html", input_text=input_text, output_text=label_text)
+    # else:
+    #     return render_template("index.html")
+
     if request.method == "POST":
         input_text = request.form['description']
         output = predict_categories(input_text)[0]
-        confidence_list = output['confidences']
-        labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.5 ]
-        label_text = ""
-        for idx, label in enumerate(labels):
-            label_text = label_text + label
-            if idx != len(labels)-1: label_text = label_text + ", "
+        sorted_confidences = sorted(output['confidences'], key=lambda x: x['confidence'], reverse=True)
+        top_classes = [elem['label'] for elem in sorted_confidences[:3]]  # Get the top 3 classes
+        label_text = ", ".join(top_classes)
         return render_template("result.html", input_text=input_text, output_text=label_text)
     else:
         return render_template("index.html")
