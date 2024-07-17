85064b3cb1341bc59beb9d6fa54583cce74e8b78
Armanul Alam <65443530+armanulalam@users.noreply.github.com>
2024-07-18 02:21:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    # if request.method == "POST":
-    #     input_text = request.form['description']
-    #     output = predict_categories(input_text)[0]
-    #     confidence_list = output['confidences']
-    #     labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.5 ]
-    #     label_text = ""
-    #     for idx, label in enumerate(labels):
-    #         label_text = label_text + label
-    #         if idx != len(labels)-1: label_text = label_text + ", "
-    #     return render_template("result.html", input_text=input_text, output_text=label_text)
-    # else:
-    #     return render_template("index.html")
-
     if request.method == "POST":
         input_text = request.form['description']
         output = predict_categories(input_text)[0]
